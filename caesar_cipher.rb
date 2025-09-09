4b16e34b756365c47e3d4aa0e30dce1c7b5022ec
jamato01 <jarrett.m.torres@gmail.com>
2025-09-09 15:14:31 -0400

    Play with RuboCop

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,18 +1,18 @@
-LOWERCASE_LETTERS = ('a'..'z')
+# frozen_string_literal: true
+
+LOWERCASE_LETTERS = ('a'..'z').freeze
 
 def caesar_cipher(string, shift_factor)
-  string_array = string.split('')
+  string_array = string.chars
   string_array.each_with_index do |character, index|
-    if LOWERCASE_LETTERS.include?(character.downcase)
-      shift_factor.times { character = character.next }
-    end
+    shift_factor.times { character = character.next } if LOWERCASE_LETTERS.include?(character.downcase)
     string_array[index] = character[-1]
   end
   string_array.join
 end
 
-puts "What is the string to cipher?"
+puts 'What is the string to cipher?'
 string_to_cipher = gets.chomp
-puts "What is the right shift factor?"
+puts 'What is the right shift factor?'
 shift_factor = gets.to_i
 puts caesar_cipher(string_to_cipher, shift_factor)
